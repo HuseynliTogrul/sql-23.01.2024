7895452fd1cd077baaea0e635e682acce61974c7
HuseynliTogrul <toghrulih@code.edu.az>
2024-01-23 14:04:15 +0400

    sql task

---
+++ SQLQuery1.sql
@@ -0,0 +1,84 @@
+--CREATE DATABASE Academy
+
+--USE ACADEMY
+
+--CREATE TABLE Students
+--(
+--	[NAME] NVARCHAR(20),
+--	SURNAME NVARCHAR(20),
+--	AGE INT,
+--	[GROUP] INT
+--)
+
+
+--DROP TABLE Students
+
+
+--INSERT INTO Students 
+--VALUES
+--('Elsad','Memmedli',32,237),('Kamran','Hesenov',20,237),('Murad','Aydinov',19,237)
+
+
+--DELETE Students
+--WHERE [NAME] = 'Elsad'
+
+
+--UPDATE Students
+--SET AGE = 15
+--WHERE AGE = 20
+
+
+--CREATE TABLE Teachers
+--(
+--	[NAME] NVARCHAR(20),
+--	SURNAME NVARCHAR(20),
+--	[GROUP] INT
+--)
+
+
+--DROP TABLE Teachers
+
+
+--INSERT INTO Teachers
+--VALUES
+--('Ahad','Taghiyev',25),('Huseyn','Mirzeyev',25)
+
+
+--DELETE Teachers
+--WHERE [NAME] = 'Ahad'
+
+
+--UPDATE Teachers
+--SET SURNAME = 'Hesenov'
+--WHERE SURNAME = 'Mirzeyev'
+
+
+--CREATE TABLE Teachers
+--(
+--	[NAME] NVARCHAR(20),
+--	AGE INT
+--)
+
+
+--INSERT INTO Teachers
+--VALUES
+--('Ahad',25)
+
+
+--DROP TABLE Teachers
+
+
+--CREATE TABLE Teachers
+--(
+--	[Name] NVARCHAR(20),
+--	AGE INT
+--)
+
+
+--INSERT INTO Teachers
+--VALUES
+--('AHAD',24),('HUSEYN',25)
+
+
+--SELECT * FROM Teachers
+--WHERE AGE = 24
